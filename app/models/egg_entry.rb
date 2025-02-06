1088798a893907b541319b68f26cecb2f68f7eaa
Amanda <privyet.amanda@gmail.com>
2025-02-06 12:40:43 -0600

    updating relationship in EggEntry class, wrapping egg entry in a div/fieldset to prep for dynamic rendering (keeps each ee visually separated); updating collection_entry#show so that only its own egg entries are displayed in it (logic in view, will move)

--- app/models/egg_entry.rb
+++ app/models/egg_entry.rb
@@ -1,2 +1,5 @@
 class EggEntry < ApplicationRecord
+  belongs_to :collection_entry
 end
+
+# adding in the `belongs_to` line ensures that the EggEntry class knows about the relationship it has with CollectionEntry, and also adds inverse methods to the EggEntry class, ie `egg_entry.collection_entry`
