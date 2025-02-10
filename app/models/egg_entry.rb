7816e373ca08d77b9640bdc9335f366361c5ed6f
Amanda <privyet.amanda@gmail.com>
2025-02-10 10:24:59 -0600

    creating relationship between ee and chicken; removing ee_id line in _ce partial (redundant, because ee title already has ee_id interpolated); replacing chicken id line with chicken name

--- app/models/egg_entry.rb
+++ app/models/egg_entry.rb
@@ -1,5 +1,6 @@
 class EggEntry < ApplicationRecord
   belongs_to :collection_entry
+  belongs_to :chicken
 end
 
 # adding in the `belongs_to` line ensures that the EggEntry class knows about the relationship it has with CollectionEntry, and also adds inverse methods to the EggEntry class, ie `egg_entry.collection_entry`
