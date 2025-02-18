5192a3fa5121c033e874725406c104821de5e0bf
Amanda Jones <privyet.amanda@gmail.com>
2025-02-18 16:18:12 -0600

    savepoint mid-implementation of reconnecting ee to ce bc I want to check out main to inspect a previous version of the form

--- app/models/collection_entry.rb
+++ app/models/collection_entry.rb
@@ -1,7 +1,6 @@
 class CollectionEntry < ApplicationRecord
   has_many :egg_entries, dependent: :destroy
   accepts_nested_attributes_for :egg_entries,
-    reject_if: -> (attributes) { attributes[:egg_count].blank? },
     allow_destroy: true
 end
 
