687a7d97f58dd8e46a639d2e2d70c6f27f4d2dbf
Amanda Jones <privyet.amanda@gmail.com>
2025-02-25 10:45:31 -0600

    adding 'Today' view - skeleton only

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   resources :chickens
-  resources :collection_entries
+  resources :collection_entries do
+    collection do
+      get :today
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
