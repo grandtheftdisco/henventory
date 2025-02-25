36377edca8ebaccb20636ad50fc3cf03a5d9a72f
Amanda Jones <privyet.amanda@gmail.com>
2025-02-25 10:42:20 -0600

    Revert "adding 'Today' view - skeleton only"
    This reverts commit 986c40d91b6c9e010e8ec78916af704a10b3e463.

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,7 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   resources :chickens
-  resources :collection_entries do
-    collection do
-      get :today
-    end
-  end
+  resources :collection_entries
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
