493410137cc23ce78436a0d01092917d8867a4ad
Amanda Jones <privyet.amanda@gmail.com>
2025-02-27 20:43:46 -0600

    styling login, post-login landing, & forgot password pages; adjusting which menu items are accessible to unauthenticated users; adding notes in passwords & sessions controllers for reference

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,9 +2,14 @@ class PasswordsController < ApplicationController
   allow_unauthenticated_access
   before_action :set_user_by_token, only: %i[ edit update ]
 
+  def show
+  end
+
+  # renders password recovery form
   def new
   end
 
+  # sends password reset email using PasswordsMailer.rest
   def create
     if user = User.find_by(email_address: params[:email_address])
       PasswordsMailer.reset(user).deliver_later
@@ -13,9 +18,12 @@ class PasswordsController < ApplicationController
     redirect_to new_session_path, notice: "Password reset instructions sent (if user with that email address exists)."
   end
 
+  # renders password edit form if the correct token is present in the URL
+  # if token is not present or invalid, user gets redirected to #new
   def edit
   end
 
+  # updates password, as long as password & password confirmation match
   def update
     if @user.update(params.permit(:password, :password_confirmation))
       redirect_to new_session_path, notice: "Password has been reset."
