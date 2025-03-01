3f2a62ae5d2cb6fc38d589eb623224deae756c8d
Amanda Jones <privyet.amanda@gmail.com>
2025-02-28 18:47:06 -0600

    adding validation for EE's egg_count; adding pseudocode for 2 egg max validation

--- app/models/egg_entry.rb
+++ app/models/egg_entry.rb
@@ -2,7 +2,16 @@ class EggEntry < ApplicationRecord
   belongs_to :collection_entry
   belongs_to :chicken
 
-  # validation for blank EEs
+  # rejects EEs with an egg_count of 0 (ie, unchecked box)
+  validates :egg_count, acceptance: true
+
+  # validation for 2 egg max
+  validate :only_2_eggs_max_per_day_per_chicken!
+
+  def only_2_eggs_max_per_day_per_chicken!
+    # sum the total # of eggs logged for this chicken today
+    # if total exceeds 2, throw error to prevent submission of form
+  end
 
 end
 
