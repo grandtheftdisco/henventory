f5f8b07ed4a2bcd44ac9dec971da38fd4eca1046
Amanda Jones <privyet.amanda@gmail.com>
2025-03-05 14:31:07 -0600

    removing egg_count validation in EggEntry model; using hidden_field to hard-code egg_count instead (rationale: creating an egg entry will always mean 1 egg was collected so this way, egg_count is automatically set to 1 by nature of an egg_entry being logged)

--- app/models/egg_entry.rb
+++ app/models/egg_entry.rb
@@ -2,10 +2,6 @@ class EggEntry < ApplicationRecord
   belongs_to :collection_entry
   belongs_to :chicken
 
-  # rejects EEs with an egg_count of 0 (ie, unchecked box)
-  # BUG - this validation is not recognizing when the box is checked....
-  validates :egg_count, acceptance: true
-
   # validation for 2 egg max
   validate :only_2_eggs_max_per_day_per_chicken!
 
