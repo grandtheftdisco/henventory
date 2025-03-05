980eea7394461ea5f1c7719637d01d8e91e66f55
Amanda Jones <privyet.amanda@gmail.com>
2025-03-05 14:33:39 -0600

    merging stash

--- app/models/egg_entry.rb
+++ app/models/egg_entry.rb
@@ -2,6 +2,13 @@ class EggEntry < ApplicationRecord
   belongs_to :collection_entry
   belongs_to :chicken
 
+<<<<<<< Updated upstream
+=======
+  # rejects EEs with an egg_count of 0 (ie, unchecked box)
+  # BUG - this validation is not recognizing when the box is checked....
+  # validates :egg_count, acceptance: true
+
+>>>>>>> Stashed changes
   # validation for 2 egg max
   validate :only_2_eggs_max_per_day_per_chicken!
 
