be845e09bdb98de8cc8c6be93e7e3ccd5d27881e
Amanda Jones <privyet.amanda@gmail.com>
2025-03-05 17:24:25 -0600

    fixing syntax of sql in sorting of collection entries; creating savepoint for progress on 2-egg-max validation (not complete but steps 1, 2b, and 3 all work in console trials & I hadn't committed any progress on this validation yet, so I wanted to save progress

--- app/models/egg_entry.rb
+++ app/models/egg_entry.rb
@@ -2,19 +2,39 @@ class EggEntry < ApplicationRecord
   belongs_to :collection_entry
   belongs_to :chicken
 
-<<<<<<< Updated upstream
-=======
-  # rejects EEs with an egg_count of 0 (ie, unchecked box)
-  # BUG - this validation is not recognizing when the box is checked....
-  # validates :egg_count, acceptance: true
-
->>>>>>> Stashed changes
   # validation for 2 egg max
   validate :only_2_eggs_max_per_day_per_chicken!
 
-  def only_2_eggs_max_per_day_per_chicken!
-    # sum the total # of eggs logged for this chicken today
-    # if total exceeds 2, throw error to prevent submission of form
+    def only_2_eggs_max_per_day_per_chicken!
+    # TASK 1
+
+    # ✔ --- 1. get array of current household's chickens.
+    household_chickens = collection_entry.user.household.chickens
+
+    # 2. find the chicken in question via chicken_id, call that current_chicken_id
+    # "undefined method `chicken` for an instance of ActiveRecord::Associations::CollectionProxy"
+    # left off here 
+    current_chicken_id = collection_entry.egg_entries.chicken.id
+
+    # ✔ --- 2b. filter for today's EEs only
+    # TODO - how to scope for household ?
+    todays_egg_entries = EggEntry.where("created_at > ? AND created_at < ?", DateTime.now.localtime.beginning_of_day, DateTime.now.localtime.end_of_day)
+
+    # ✔ --- 3. once you have current_chicken_id, use it to search for egg entries within todays_egg_entries that have the same chicken id
+    todays_egg_entries_for_chicken = todays_egg_entries.where(chicken_id: current_chicken_id)
+
+    # TASK 2
+
+    # 4. step #3 will either yield a) an empty array or b) an array of egg entries.
+
+    # 4a. if the array is empty go ahead and save the ee
+    if todays_egg_entries_for_chicken.length <= 1
+      return
+    elsif todays_egg_entries_for_chicken.length >= 2
+      # throw error
+      errors.add(:current_chicken_id, 'can\'t lay any more eggs today 😴🐔')
+    end
+
   end
 
   validate :household_owns_chicken!
