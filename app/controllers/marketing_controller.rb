97062d1f404495edf733e6ac29d44aba2176192d
Amanda Jones <privyet.amanda@gmail.com>
2025-03-06 16:08:03 -0600

    correcting 'date' to 'created_at'

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -3,6 +3,6 @@ class MarketingController < ApplicationController
     # remember to scope to current household once that PR is approved!
     @collection_entries = Current.household.collection_entries.includes(egg_entries: :chicken)
     .where(created_at: Time.current.localtime.beginning_of_day..Time.current.localtime.end_of_day)
-    .order("id DESC")
+    .order("created_at DESC")
   end
 end
