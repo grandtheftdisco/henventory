56b55dd3207b5e6660d1658814d68980a3d3d736
Amanda Jones <privyet.amanda@gmail.com>
2025-03-05 19:50:25 -0600

    implementing v1 of 2-egg-max validation; the search for current day's egg entries is not scoped to household yet, but it works in a vacuum in dev environment where no other households have logged ees for the current day

--- app/models/egg_entry.rb
+++ app/models/egg_entry.rb
@@ -2,45 +2,37 @@ class EggEntry < ApplicationRecord
   belongs_to :collection_entry
   belongs_to :chicken
 
-  validate :only_2_eggs_max_per_day_per_chicken!
-
-    def only_2_eggs_max_per_day_per_chicken!
-    # TASK 1
-
-    # ✔ --- 1. get array of current household's chickens.
-    household_chickens = collection_entry.user.household.chickens
-
-    # 2. find the chicken in question via chicken_id, call that current_chicken_id
-    # "undefined method `chicken` for an instance of ActiveRecord::Associations::CollectionProxy"
-    # left off here 
-    current_chicken_id = collection_entry.egg_entries.chicken.id
-
-    # ✔ --- 2b. filter for today's EEs only
-    # TODO - how to scope for household ?
-    todays_egg_entries = EggEntry.where("created_at > ? AND created_at < ?", DateTime.now.localtime.beginning_of_day, DateTime.now.localtime.end_of_day)
-
-    # ✔ --- 3. once you have current_chicken_id, use it to search for egg entries within todays_egg_entries that have the same chicken id
-    todays_egg_entries_for_chicken = todays_egg_entries.where(chicken_id: current_chicken_id)
-
-    # TASK 2
-
-    # 4. step #3 will either yield a) an empty array or b) an array of egg entries.
-
-    # 4a. if the array is empty go ahead and save the ee
-    if todays_egg_entries_for_chicken.length <= 1
-      return
-    elsif todays_egg_entries_for_chicken.length >= 2
-      # throw error
-      errors.add(:current_chicken_id, 'can\'t lay any more eggs today 😴🐔')
-    end
-
-  end
-
   validate :household_owns_chicken!
-
+  validate :only_2_eggs_max_per_day_per_chicken!
+  
   def household_owns_chicken!
     collection_entry.user.household.chickens.find(chicken_id)
   rescue ActiveRecord::RecordNotFound
     errors.add(:chicken_id, 'must be owned by household')
+  end
+
+  def only_2_eggs_max_per_day_per_chicken!
+    # array of the chicken_ids of any chickens with egg_entries inside the current collection entry
+    ee_chicken_id_array = collection_entry.egg_entries.map do |ee|
+      ee.chicken_id
+    end
+
+    return if ee_chicken_id_array.length == 0 
+
+    # TODO - how to scope for household ?
+    todays_egg_entries = EggEntry.where("created_at > ? AND created_at < ?", DateTime.now.localtime.beginning_of_day, DateTime.now.localtime.end_of_day)
+
+    todays_egg_entries_for_chicken = []
+
+    ee_chicken_id_array.each do |chk_id|
+      todays_egg_entries_for_chicken = todays_egg_entries.where(chicken_id: chk_id)
+      if todays_egg_entries_for_chicken.length <= 1
+        return
+      elsif todays_egg_entries_for_chicken.length >= 2
+        # throw error
+        errors.add(:base, :invalid, message: '-- This chicken can\'t lay any more eggs today 😴🐔')
+      end
+    end 
+
   end
 end
