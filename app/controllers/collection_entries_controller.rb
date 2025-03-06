1ce7f4441be0ea890bf8a6a24bfa1978e694b0ac
Amanda Jones <privyet.amanda@gmail.com>
2025-03-06 16:41:20 -0600

    removing extra whitespace; switching from sorting by id to sorting by date & changing sql syntax

--- app/controllers/collection_entries_controller.rb
+++ app/controllers/collection_entries_controller.rb
@@ -16,7 +16,7 @@ class CollectionEntriesController < ApplicationController
     # remember to scope to current household once that PR is approved!
     @collection_entries = Current.household.collection_entries.includes(egg_entries: :chicken)
     .where(created_at: Time.current.localtime.beginning_of_day..Time.current.localtime.end_of_day)
-    .order("id desc")
+    .order("created_at desc")
   end
 
   # GET /collection_entries/1 or /collection_entries/1.json
