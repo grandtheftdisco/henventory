36c6a49d338ca1b1dfe3df9c74f4f467d8099e8a
Amanda Jones <privyet.amanda@gmail.com>
2025-03-06 20:21:16 -0600

    redirecting to settings view after household name change successful; styling

--- app/controllers/households_controller.rb
+++ app/controllers/households_controller.rb
@@ -29,7 +29,7 @@ class HouseholdsController < ApplicationController
   def update
     respond_to do |format|
       if @household.update(household_params)
-        format.html { redirect_to @household, notice: "Household was successfully updated." }
+        format.html { redirect_to '/settings', notice: "Household was successfully updated." }
         format.json { render :show, status: :ok, location: @household }
       else
         format.html { render :edit, status: :unprocessable_entity }
