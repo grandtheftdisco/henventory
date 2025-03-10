64c185a354e2794777d9576fcf8f683cff92f464
Amanda Jones <privyet.amanda@gmail.com>
2025-03-10 16:56:15 -0500

    modifying User table to include 'mode' column for toggling between flock & layer mode on settings view; conditionalizing collection entry partial & collection entry form based on user.mode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_26_232826) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_10_212412) do
   create_table "chickens", force: :cascade do |t|
     t.string "name"
     t.string "breed"
