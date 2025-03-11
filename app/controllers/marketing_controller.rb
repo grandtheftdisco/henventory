728f4734e9169fe181f248ab6306bf477cc7e335
Amanda Jones <privyet.amanda@gmail.com>
2025-03-10 21:13:50 -0500

    changing allow_unauthenticated_access line so that action name is a symbol; now the routing works!

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -1,4 +1,5 @@
 class MarketingController < ApplicationController
+  allow_unauthenticated_access only: [ :faq ]
   def home
     @collection_entries = Current.household.collection_entries.includes(egg_entries: :chicken)
     .where(created_at: Time.current.localtime.beginning_of_day..Time.current.localtime.end_of_day)
