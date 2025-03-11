c78dcdace66a8cd7d3713aa668ca705d438b1258
Amanda Jones <privyet.amanda@gmail.com>
2025-03-10 21:37:56 -0500

    writing outline for copy to go in faq page; attempting to create a link to user settings to toggle egg tracking mode - this is more of a savepoint to remember what I've tried already, because I think tomorrow I'll try to create a toggle on the settings page itself rather than creating an entirely new view

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   post '/users' => 'users#create'
   get '/settings' => 'marketing#settings'
   get '/faq' => 'marketing#faq'
+  get '/users/:id/edit' => 'users#edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
