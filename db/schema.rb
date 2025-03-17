21364f1867dfa8568197fb2d4a4255e33a2092c9
Amanda Jones <privyet.amanda@gmail.com>
2025-03-17 13:49:30 -0500

    making changes to test pg swap on local server

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_02_26_232826) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "chickens", force: :cascade do |t|
     t.string "name"
     t.string "breed"
