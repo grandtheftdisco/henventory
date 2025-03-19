c7cee96ab66a4860e480ba95e407844ca6011743
Amanda Jones <privyet.amanda@gmail.com>
2025-03-19 16:13:14 -0500

    overriding default behavior of to_param to use :invite_token instead of :id in household urls

--- app/models/household.rb
+++ app/models/household.rb
@@ -3,4 +3,9 @@ class Household < ApplicationRecord
   has_many :chickens
   has_many :collection_entries
   has_secure_token :invite_token # to regenerate, use `#regenerate_invite_token`
+
+  # overrides default behavior of to_param so that :invite_token is used instead of :id
+  def to_param
+    invite_token
+  end
 end
