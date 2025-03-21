f3e4f91101a397fe338f9e5a26254e08202a7ebe
Bookis Worthy <bookis.worthy@gmail.com>
2025-03-21 11:06:28 -0700

    Clean up style

--- app/models/collection_entry.rb
+++ app/models/collection_entry.rb
@@ -1,9 +1,6 @@
 class CollectionEntry < ApplicationRecord
   has_many :egg_entries, dependent: :destroy
-  accepts_nested_attributes_for :egg_entries,
-    allow_destroy: true
+  accepts_nested_attributes_for :egg_entries, allow_destroy: true
   belongs_to :household
   belongs_to :user
 end
-
-# `allow_destroy: true` allows users to delete EEs
