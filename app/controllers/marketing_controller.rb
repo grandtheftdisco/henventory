bbe1b538519a53c09d667764e83b4da3128f0de1
Bookis Worthy <bookis.worthy@gmail.com>
2025-03-21 10:58:19 -0700

    Remove unused Current user check since the before_action checks for auth

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -2,10 +2,8 @@ class MarketingController < ApplicationController
   allow_unauthenticated_access only: [ :how_it_works, :acknowledgements, :faq ]
 
   def home
-    if Current.user
-      @collection_entries = Current.household.collection_entries.includes(egg_entries: :chicken)
-      .where(created_at: Time.current.localtime.beginning_of_day..Time.current.localtime.end_of_day)
-    end
+    @collection_entries = Current.household.collection_entries.includes(egg_entries: :chicken)
+    .where(created_at: Time.current.localtime.beginning_of_day..Time.current.localtime.end_of_day)
   end
 
   def faq
