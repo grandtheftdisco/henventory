6aee37eee29ef93084c5aa07aaf28bf0903a97f6
Amanda Jones <privyet.amanda@gmail.com>
2025-03-25 14:58:40 -0500

    patching production with current user arg until I can figure out why edit_user_path is no longer a sufficient path to allow someone to edit their page -- something changed when I repurposed user#show for the settings landing instead of users#settings

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :collection_entries do
     collection do
       get :today
-    end
+    end   
   end
   resources :households
   get '/signup' => 'users#new'
