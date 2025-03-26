c6c2be502c97b711b3e2302ce397849080ff89e7
Amanda Jones <privyet.amanda@gmail.com>
2025-03-26 11:13:52 -0500

    changing root to marketing#hello to create a public/unauth homepage; conditionalizing the path of the header in layouts/application so that it brings auth'd users to the post-login-landing and unauth'd users to root

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   get '/collection_entries/today' => 'collection_entries#today', as: :today
   get '/how_it_works' => 'marketing#how_it_works', as: :how_it_works
   get '/acknowledgements' => 'marketing#acknowledgements', as: :acknowledgements
+  get '/home' => 'marketing#home', as: :landing
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -29,5 +30,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-root "marketing#home"
+root "marketing#hello"
 end
