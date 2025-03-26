8e1bfd895790bab20ddda985059b3ea22825aaf4
Amanda Jones <privyet.amanda@gmail.com>
2025-03-26 16:25:37 -0500

    writing method to set the user's time zone per request, using their local time zone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
 
   allow_browser versions: :modern
+  around_action :set_time_zone
 
   # return the start and end limits of the collection as a 2 items array
   def pagy_calendar_period(collection)
@@ -16,4 +17,14 @@ class ApplicationController < ActionController::Base
     collection.where(created_at: from...to)
   end
 
+  private
+
+  def set_time_zone
+    old_time_zone = Time.zone
+    @local_time_zone = Current.user.household.time_zone
+    Time.zone = @local_time_zone
+    yield
+  ensure
+    Time.zone = old_time_zone
+  end
 end
