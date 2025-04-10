390491645f74d1be2c89c1ea458826d9e316d2c4
Amanda Jones <privyet.amanda@gmail.com>
2025-04-09 19:22:18 -0500

    adding current user's time zone as an argument to pagy_calendar_period method in ApplicationController -- this solved the sorting bug and now all entries are being sorted by local dates/times instead of by GMT; setting local time zone in CollectionEntriesController actions, but wrapping it in a method for easy changeability in the future; abandoning the strategy to set the time zone in the ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,27 +3,16 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
 
   allow_browser versions: :modern
-  around_action :set_time_zone if Current.user
 
   # return the start and end limits of the collection as a 2 items array
   def pagy_calendar_period(collection)
     starting = collection.minimum(:created_at) || Time.current
     ending   = collection.maximum(:created_at) || Time.current
-    [starting.in_time_zone, ending.in_time_zone]
+    [starting.in_time_zone(Current.user.household.time_zone), ending.in_time_zone(Current.user.household.time_zone)]
   end
 
   # return the collection filtered by a time period
   def pagy_calendar_filter(collection, from, to)
     collection.where(created_at: from...to)
-  end
-
-  private
-
-  def set_time_zone
-    if logged_in?
-      Time.use_zone(Current.user.household.time_zone) { yield }
-    else 
-      yield
-    end
   end
 end
