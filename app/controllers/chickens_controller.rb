21b9bfb47725792a78df06467430524dbe9a9c1c
Amanda Jones <privyet.amanda@gmail.com>
2025-05-12 11:03:56 -0500

    Amanda/Chicken & Collection Entry View Updates (#106)
    * refining styling of collection entry partials; expanding chicken stats on chickens#show; linking chicken's name on collection entry partials to that chicken's #show view.
    
    * conditionalizing the eggs-per-week stat to only show up after 2 weeks of egg-laying to fix a bug where chickens with only a couple of eggs were getting large numbers for their averages (ie, '18 eggs per week' and even as high as 1,614).
    
    * adding chicken stat to display how long it's been since a given chicken has laid an egg
    
    * moving 'days since last egg' text to top of card; adding borders to separate chicken stats to add visual interest/assist readability
    
    * fixing broken link to chicken stat card from collection entry
    
    * removing running total of eggs since it's not as useful as averages or short-term stats; adding running monthly total instead
    
    * conditionalizing title of header for chickens#index based on any params present to filter the chickens
    
    * moving collection entry edit & delete options bar into collection entry partial
    
    * deleting obsolete ce#show view; removing links to 'Details' from collection entry #today & #index views; removing 'show this entry' from ce#edit and changing 'back to all' to 'back to today's entries'
    
    * conditionalizing the appearance of 'view my pullets' menu item
    
    * refactoring chicken category options to look more like a classic filter-by menu
    
    * redirecting to Today's Entries view after CE is created, since there's no longer a need for CE#show

--- app/controllers/chickens_controller.rb
+++ app/controllers/chickens_controller.rb
@@ -3,18 +3,28 @@ class ChickensController < ApplicationController
 
   def index
     @chickens = Current.household.chickens.where.not(status: :expired)
+    @filtered_title = "My Flock"
+    @pullets = Current.household.chickens.where(status: :pullet)
 
     # filtering of view based on chicken status
     if params[:pullets]
       @chickens = @chickens.where(status: :pullet)
+      @filtered_title = "My Pullets"
     elsif params[:expired]
       @chickens = @chickens.rewhere(status: :expired)
     elsif params[:layers]
       @chickens = @chickens.where(status: :layer)
+      @filtered_title = "My Layers"
     end
   end
 
   def show
+    if @chicken.egg_entries.exists?
+      @days_since_first_egg = (Time.now - @chicken.egg_entries.first.created_at) / (60*60*24)
+      @weeks_since_first_egg = @days_since_first_egg / 7
+      @days_since_last_egg = ((Time.now - @chicken.egg_entries.last.created_at) / (60*60*24)).truncate
+      @eggs_laid_this_month = @chicken.egg_entries.where(created_at: Time.current.localtime.beginning_of_month..Time.current.localtime).sum(:egg_count)
+    end
   end
 
   def new
