e71869718aa56119a887298d910f68ea10b3ac63
Amanda <grandtheftdisco@gmail.com>
2025-11-13 10:27:18 -0600

    basic timepicker config setup; removing debug lines from stimulus controller

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -2,40 +2,8 @@ import Flatpickr from "stimulus-flatpickr"
 
 export default class extends Flatpickr {
   connect() {
-    console.log("Flatpickr controller connected!")
-    console.log("Element:", this.element)
-    console.log("Data attributes:", this.element.dataset)
+    console.log("Flatpickr controller connected")
     super.connect()
-    console.log("Flatpickr initialized")
-    console.log("Flatpickr instance (this.fp):", this.fp)
-    console.log("Config:", this.config)
-
-    // Try to manually open it for debugging
-    if (this.fp) {
-      console.log("FP methods available:", Object.keys(this.fp))
-      console.log("Calendar container:", this.fp.calendarContainer)
-      console.log("Is calendar in DOM?", document.body.contains(this.fp.calendarContainer))
-      console.log("isMobile:", this.fp.isMobile)
-
-      // Try to force open for testing
-      setTimeout(() => {
-        console.log("Attempting to force open calendar...")
-        this.fp.open()
-
-        // Check CSS
-        const container = this.fp.calendarContainer
-        const styles = window.getComputedStyle(container)
-        console.log("Calendar display:", styles.display)
-        console.log("Calendar visibility:", styles.visibility)
-        console.log("Calendar opacity:", styles.opacity)
-        console.log("Calendar position:", styles.position)
-        console.log("Calendar top:", styles.top)
-        console.log("Calendar left:", styles.left)
-        console.log("Has 'open' class?", container.classList.contains('open'))
-      }, 1000)
-    } else {
-      console.error("ERROR: Flatpickr instance (this.fp) is undefined!")
-    }
   }
 
   disconnect() {
